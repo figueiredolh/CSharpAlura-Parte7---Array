1dc47d1a591222be81d12750abd3933e22e62959
Henrique Figueiredo <henricklima94@gmail.com>
2022-03-13 14:12:18 -0300

    Criação classe ContaCorrente e primeiros testes

---
+++ ContaCorrente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    public class ContaCorrente
+    {
+        public int Agencia { get; private set; }
+        public int Conta { get; private set; }
+        public ContaCorrente(int agencia, int conta)
+        {
+            Agencia = agencia;
+            Conta = conta;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Criação de lista de conta corrente (array de ContaCorrente) em Program.cs
+
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                new ContaCorrente(45, 12345),
+                new ContaCorrente(80, 67812),
+                new ContaCorrente(23, 34457)
+            };
+
+            for(int i = 0; i < contas.Length; i++)
+            {
+                Console.WriteLine($"Agência: {contas[i].Agencia} || Conta: {contas[i].Conta}");
+            }
+        }
+    }
+}
